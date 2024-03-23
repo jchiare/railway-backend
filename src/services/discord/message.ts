24419748723fcc6fb6592333e736d2aa1bf045ff
Jay Chiarella <jay.gch93@gmail.com>
2024-03-23 21:30:25 +0100

    expand data in messages

--- src/services/discord/message.ts
+++ src/services/discord/message.ts
@@ -8,9 +8,17 @@ export class DiscordMessage {
 
   public handleStream(message: Message): void {
     const clientId = Date.now().toString();
+
+    // don't send messages with no content
+    if (!message.content) return;
+
     const data = JSON.stringify({
       content: message.content,
-      author: message.author.username
+      author: message.author.username,
+      userAvatar: message.author.avatarURL(),
+      createdAt: message.createdAt,
+      linkToMessage: `https://discord.com/channels/${message.guild?.id}/${message.channel.id}/${message.id}`,
+      inThread: message?.position != null && message?.position >= 0
     });
 
     this.res.write("event: message\n");
